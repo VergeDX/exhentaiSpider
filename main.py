2958ec64722c594e0657ee10149c316e416c02a5
VergeDX <wxy20001225@126.com>
2020-07-14 21:52:09 +0800

    [M] simplify the code

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ if __name__ == '__main__':
         config_toml = toml.loads(config_file.read())
         config_file.close()
 
-        igneous = config_toml.get('igneous')
-        ipb_member_id = config_toml.get('ipb_member_id')
-        ipb_pass_hash = config_toml.get('ipb_pass_hash')
-
-        if not (igneous and ipb_member_id and ipb_pass_hash):
-            print('cannot found "igneous", "ipb_member_id" or "ipb_pass_hash" in config.toml file.')
-            exit(-1)
-
         proxies = {
             'http': '127.0.0.1:8118',
             'https': '127.0.0.1:8118',
@@ -28,12 +20,8 @@ if __name__ == '__main__':
                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
                               'Chrome/83.0.4103.116 Safari/537.36'})
 
-            try:
-                r = session.get('https://exhentai.org/?page=' + str(index) + '&f_search=uploader:BlossomPlus',
-                                proxies=proxies, cookies=config_toml)
-            except ConnectionResetError:
-                pass
-
+            r = session.get('https://exhentai.org/?page=' + str(index) + '&f_search=uploader:BlossomPlus',
+                            proxies=proxies, cookies=config_toml)
             soup = BeautifulSoup(r.text, 'html.parser')
 
             for tag in soup.findAll('a', href=True):
